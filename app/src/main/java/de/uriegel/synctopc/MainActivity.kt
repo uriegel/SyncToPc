dbb299643d72d418e5c59bdb4861434187b092e0
Uwe Riegel <uriegel@hotmail.de>
2021-12-29 12:20:18 +0100

    Initial commit

---
+++ app/src/main/java/de/uriegel/synctopc/MainActivity.kt
@@ -0,0 +1,18 @@
+package de.uriegel.synctopc
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},READ_EXTERNAL_STORAGE);
+    }
+}
