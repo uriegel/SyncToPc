ffcf7ee056b13a0ea4b6a832cc43878c4e406e12
Uwe Riegel <uriegel@hotmail.de>
2021-12-29 16:18:04 +0100

    ManageAllFiles access

--- app/src/main/java/de/uriegel/synctopc/MainActivity.kt
+++ app/src/main/java/de/uriegel/synctopc/MainActivity.kt
@@ -1,18 +1,58 @@
 package de.uriegel.synctopc
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.os.Environment
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import de.uriegel.activityextensions.ActivityRequest
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), CoroutineScope {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},READ_EXTERNAL_STORAGE);
+        launch {
+            val backgroundResult =
+                activityRequest.checkAndAccessPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+            if (backgroundResult.any { !it.value }) {
+                toast(R.string.no_access, Toast.LENGTH_LONG)
+                finish()
+                return@launch
+            }
+
+            if (Build.VERSION.SDK_INT >= 30 && !hasAllFilesPermission()) {
+                val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+                startActivity(Intent( Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri))
+            }
+
+            //val path = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera"
+            val path = Environment.getExternalStorageDirectory().toString() + "/Pictures"
+            Log.d("Files", "Path: $path")
+            val directory = File(path)
+            val files = directory.listFiles()
+            Log.d("Files", "Size: " + files?.size)
+            for (i in files!!.indices) {
+                Log.d("Files", "FileName:" + files[i].name)
+            }
+        }
     }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun hasAllFilesPermission() = Environment.isExternalStorageManager()
+
+    override val coroutineContext = Dispatchers.Main
+
+    private val activityRequest = ActivityRequest(this)
 }
---
+++ app/src/main/java/de/uriegel/synctopc/Toasts.kt
@@ -0,0 +1,9 @@
+package de.uriegel.synctopc
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+fun Context.toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, this.resources.getText(resId), duration).show()
+}
